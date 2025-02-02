c2e0d5ef10c9ac414a68e93c6c9453656c445f8d
ParasRajput810 <paraswork810@gmail.com>
2025-02-02 14:04:34 +0530

    Added K8s configuration

--- terra/ec2.tf
+++ terra/ec2.tf
@@ -7,10 +7,13 @@ resource "aws_instance" "PRT_ins" {
   ami                         = "ami-0e1bed4f06a3b463d"
   key_name                    = aws_key_pair.my_key.key_name
   depends_on                  = [aws_vpc.myvpc, aws_internet_gateway.myigw, aws_subnet.public-subnet]
+
+  tags = {
+    Name = count.index == 0 ? "Master" : "Slave${count.index}"
+  }
 }
 
 resource "aws_key_pair" "my_key" {
   key_name   = "my-ssh-key"
   public_key = file("./ansi/id_rsa.pub")
-
 }
